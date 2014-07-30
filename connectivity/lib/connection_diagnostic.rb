d52cf4307d7b7707f92c583834ac7f9a5daddc9d
dario-c <dariocavanillas@gmail.com>
2014-07-30 11:11:23 +0200

    Connection_diagnostic checks if Client is online or not

--- connectivity/lib/connection_diagnostic.rb
+++ connectivity/lib/connection_diagnostic.rb
@@ -1,6 +1,16 @@
 require 'client'
 
 class ConnectionDiagnostic
+
+  attr_accessor :client
+
+  def initialize(client)
+    @client = client
+  end
+
+  def check_status
+    @client.online_status ? "AT#UD" : "Disconnected"
+  end
   # perform_diagnostic
   # disconnects the client
   # tries to connect the client 3 times
@@ -8,4 +18,6 @@ class ConnectionDiagnostic
   # if successful sends 'AT#UD' to client (diagnostic message)
   #   and receives from the client the diagnostic info
   #   this info should be accesible on the attribute info
+
+
 end
--- connectivity/spec/connection_diagnostic_spec.rb
+++ connectivity/spec/connection_diagnostic_spec.rb
@@ -2,5 +2,38 @@ require 'spec_helper'
 
 describe ConnectionDiagnostic do
 
+  it "Check that we still know how to do a basic mock" do
+    client = double('client')
+    allow(client).to receive(:online_status).and_return(false)
+
+    expect(client.online_status).to eql(false)
+  end
+
+  it "ConnectionDiagnostic receives a Status" do
+    client = Client.new
+    connection_diagnostic = ConnectionDiagnostic.new(client)
+ 
+    expect(connection_diagnostic.client).to eq(client)
+  end
+
+  it "Is able to see if the Client is Disconnected" do
+    client = double('client')
+    allow(client).to receive(:online_status).and_return(false)
+
+    connection_diagnostic = ConnectionDiagnostic.new(client)
+
+    expect(connection_diagnostic.check_status).to eq("Disconnected")
+  end
+
+    
+    it "Is able to see if the Client is connected" do
+    client = double('client')
+    allow(client).to receive(:online_status).and_return(true)
+
+    connection_diagnostic = ConnectionDiagnostic.new(client)
+
+    expect(connection_diagnostic.check_status).to eq("AT#UD")
+  end  
+
 
 end
